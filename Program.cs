d770150ff97f37a4abe026281413863c1c903462
Bessy Theodoropoulou <mellonance@gmail.com>
2017-09-27 00:47:10 +0300

    Add main file

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Krypto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BloomFilter bloom = new BloomFilter();
+
+            Console.WriteLine("Insert your elements:");
+            string element = Console.ReadLine();
+            bloom.AddData(element);
+            Console.WriteLine("Check if the element exists \n" + bloom.SearchValue(element).ToString());
+
+            Console.ReadKey();  
+        }
+    }
+}
